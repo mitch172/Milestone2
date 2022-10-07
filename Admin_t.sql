b0cd3eedd264723b5277ab4190d1417ab119a068
mitch172 <mitch172@go.stockton.edu>
2022-10-07 11:53:12 -0400

    SQL tables update

--- Admin_t.sql
+++ Admin_t.sql
@@ -1,6 +1,7 @@
 create table Admin_t
     -> (
     -> ID int(11) not null,
+    -> username varchar(20) not null,
     -> email varchar(20) not null,
     -> password varchar(20) not null,
     -> constraint Admin_PK primary key(ID)
--- User_t.sql
+++ User_t.sql
@@ -1,6 +1,8 @@
 create table User_t
     -> (
     -> ID int(11) not null,
+    -> first_name varchar(10),
+    -> last_name varchar(20),
     -> email varchar(20) not null,
     -> password varchar(20) not null,
     -> constraint User_PK primary key(ID)
