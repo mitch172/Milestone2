bc6e83d7fe6531764c0834ba6c411925c5f4bef4
mitch172 <mitch172@go.stockton.edu>
2022-10-09 21:28:33 -0400

    Move all SQL tables to SQL_Databases.sql

--- Admin_t.sql
+++
@@ -1,8 +0,0 @@
-create table Admin_t
-    -> (
-    -> ID int(11) not null,
-    -> username varchar(20) not null,
-    -> email varchar(20) not null,
-    -> password varchar(20) not null,
-    -> constraint Admin_PK primary key(ID)
-    -> );
